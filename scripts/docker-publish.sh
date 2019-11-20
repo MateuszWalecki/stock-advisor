d6b32648cdae8a1cd19acf05278f37ac7bf9c888
Mateusz Walecki <mateusz.walecki2@gmail.com>
2019-11-20 12:31:49 +0100

    changes due to Travis CI

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,6 +1,6 @@
 DOCKER_ENV=''
 DOCKER_TAG=''
-DOCKER_ENV=docker
+# DOCKER_ENV=docker
 
 case "$TRAVIS_BRANCH" in
   "master")
@@ -15,7 +15,7 @@ esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -f ./src/StockAdvisor.Api/Dockerfile.$DOCKER_ENV -t stock-advisor:$DOCKER_TAG ./src/StockAdvisor.Api --no-cache
+docker build -f ./src/StockAdvisor.Api/Dockerfile -t stock-advisor:$DOCKER_TAG ./src/StockAdvisor.Api --no-cache
 
 docker tag stock-advisor:$DOCKER_TAG $DOCKER_USERNAME/stock-advisor:$DOCKER_TAG
 
